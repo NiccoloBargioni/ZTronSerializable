be371da24effb1ddd83f722efb94b92a87fb2485
= <niccolo.dellarocca@gmail.com>
2024-11-10 14:37:53 +0100

    Published init of EmptyFK

--- Sources/ZTronSerializable/Serialization/SerializableForeignKeys/EmptyFK.swift
+++ Sources/ZTronSerializable/Serialization/SerializableForeignKeys/EmptyFK.swift
@@ -2,6 +2,8 @@ import Foundation
 import SQLite
 
 public final class EmptyFK: SerializableForeignKeys {
+    public init() {  }
+    
     public func validate(on db: SQLite.Connection) throws -> ForeignKey? {
         return nil
     }
