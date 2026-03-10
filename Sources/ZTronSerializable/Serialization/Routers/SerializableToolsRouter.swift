3c11b2758ed68427fc3e8fd9b0a397881dc079b1
= <niccolo.dellarocca@gmail.com>
2026-03-10 12:15:09 +0100

    Reactivated delete pass for tools that don't exist anymore

--- Sources/ZTronSerializable/Serialization/Routers/SerializableToolsRouter.swift
+++ Sources/ZTronSerializable/Serialization/Routers/SerializableToolsRouter.swift
@@ -112,7 +112,7 @@ public final class SerializableToolsRouter: SerializableNode {
             }
         }
 
-        /*
+        
         var toolsForThisTab: [String: SerializableToolNode] = [:]
         
         self.router.forEach { absolutePath, toolModel in
@@ -128,7 +128,7 @@ public final class SerializableToolsRouter: SerializableNode {
                 return toolsForThisTab[tool.getName()] == nil
             },
             shouldDecreasePositions: false
-        )*/
+        )
     }
     
     public func updateOn(db: SQLite.Connection, with foreignKeys: any SerializableForeignKeys, propagate: Bool) throws {
